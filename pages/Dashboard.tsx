bb722567bd0f027b5293bc330dbc1b41333c46b3
Kapeet <itamar246@gmail.com>
2022-06-29 13:53:54 -0700

    fixing up eslint config

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -1,51 +1,51 @@
-import {Button, Card} from '@mui/material';
-import React, {useState} from 'react';
-
+import { Button, Card } from '@mui/material';
+import React, { useState } from 'react';
 
 interface Listing {
-
-        id: string;
-        steamid: string;
-        appid: number;
-        currencies: {
-            metal: number;
-        }
-        offers: number;
-        buyout: number;
-        details: string;
-        created: number;
-        bump: number;
-        indent: number;
-        item: {
-            id: number;
-            original_id: number;
-            defindex: number;
-            level: number;
-            quality: number;
-            inventory: number;
-            quantity: number;
-            origin: number;
-            attriutes: Array<{
-                defindex: number;
-                value: number;
-                float_value?: number;
-            }>;
-            name: string;
-
-        }
-
+  id: string;
+  steamid: string;
+  appid: number;
+  currencies: {
+    metal: number;
+  };
+  offers: number;
+  buyout: number;
+  details: string;
+  created: number;
+  bump: number;
+  indent: number;
+  item: {
+    id: number;
+    original_id: number;
+    defindex: number;
+    level: number;
+    quality: number;
+    inventory: number;
+    quantity: number;
+    origin: number;
+    attriutes: Array<{
+      defindex: number;
+      value: number;
+      float_value?: number;
+    }>;
+    name: string;
+  };
 }
 interface ListingsResponse {
-    listings: Array<Listing>;
-    cap: number;
-    promotes_remaining: number
+  listings: Array<Listing>;
+  cap: number;
+  promotes_remaining: number;
 }
 
 const Dashboard: React.FC = () => {
-  const [responseObject, setResponseObject] =
-            useState<ListingsResponse | undefined>();
+  const [responseObject, setResponseObject] = useState<
+    ListingsResponse | undefined
+  >();
   const fetchListings = async () => {
-    const res = await fetch(`https://backpack.tf/api/classifieds/listings/v1?token=${process.env.API_KEY}`);
+    const res = await fetch(
+      `https://backpack.tf/api/classifieds/listings/v1?token=${process.env.API_KEY}`
+    );
+
     const json = await res.json();
 
     setResponseObject(json);
@@ -55,11 +55,10 @@ const Dashboard: React.FC = () => {
     <div>
       <Card>
         <Button onClick={fetchListings}>Fetch listings</Button>
-        {responseObject?.listings?.length && responseObject?.listings.map((l) =>
-          <li key={l.id}>
-            {l.item.name}
-          </li>,
-        )}
+        {responseObject?.listings?.length &&
+          responseObject?.listings.map((l, a) => (
+            <li key={l.id}>{l.item.name}</li>
+          ))}
       </Card>
     </div>
   );
